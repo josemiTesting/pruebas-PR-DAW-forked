e9df8a5d40c8cab86a4dc611f4ebdbe16a560157
JosemiProfessor <90213511+JosemiProfessor@users.noreply.github.com>
2024-12-12 13:34:04 +0100

    Create programa.py

---
+++ programa.py
@@ -0,0 +1,13 @@
+# Programa de ejemplo en Python para practicar el control de versiones
+
+# Funciones
+def suma(n1,n2):
+  return n1 + n2
+
+# Programa principal
+x = 5
+y = 3
+
+resultado_suma = suma(x,y)
+print(f'La suma de 5 y 3 es: {resultado_suma}')
+
